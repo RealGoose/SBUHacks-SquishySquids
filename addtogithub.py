9e455636699a884335b8f4362fc524a8c13dbfaa
Hamza Ali <hamza4259@yahoo.com>
2020-09-25 20:23:09 -0400

    Add files via upload

---
+++ addtogithub.py
@@ -0,0 +1,12 @@
+import http.client
+import mimetypes
+conn = http.client.HTTPSConnection("api.radar.io")
+payload = ''
+headers = {
+  'Authorization': 'prj_test_sk_9a8f0000f39d8e0eae541935779588195a7d3401',
+  'Cookie': '__cfduid=d6dc9a3eeb82f6b63f9ab8bbe607cfa891601077693'
+}
+conn.request("GET", "/v1/geocode/ip?ip=107.77.199.117", payload, headers)
+res = conn.getresponse()
+data = res.read()
+print(data.decode("utf-8"))
