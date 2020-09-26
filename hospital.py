fcfc562b10dc5f248e66ebbba004f972acbfc34b
Daniel Schwartz <dls422@cornell.edu>
2020-09-25 23:13:50 -0400

    added distanceRequest

--- hospital.py
+++ hospital.py
@@ -1,7 +1,7 @@
 import http.client
 import mimetypes
 import json
-
+from distanceRequest import getDistance
 def getHospList(latitude, longitude):
     """
     Prints a list of formatted addresses of nearby hopsitals
@@ -13,7 +13,7 @@ def getHospList(latitude, longitude):
     }
 
 
-    conn.request("GET", "/v1/search/autocomplete?query=hospital&near=" + str(latitude) + "," + str(longitude), payload, headers)
+    conn.request("GET", "/v1/search/autocomplete?query=hospital&near=" + str(latitude) + "," + str(longitude) + "&limit=5", payload, headers)
     res = conn.getresponse()
     data = res.read().decode("utf-8")
 
@@ -23,8 +23,13 @@ def getHospList(latitude, longitude):
     #print(json_data)
     addresses = json_data["addresses"]
 
+    d_list = []
+
     formatted_address_list = []
     for address in addresses:
         formatted_address_list.append(address["formattedAddress"])
+        #d_list.append((address["latitude"],address["longitude"]))
+        getDistance(latitude, longitude, address["latitude"], address["longitude"])
 
+    print(d_list)
     print(formatted_address_list)
