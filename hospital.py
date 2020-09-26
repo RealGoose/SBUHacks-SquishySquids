5d9c07cd49ca1e4b2ea8b27e807e12c7ddab1230
RealGoose <69604539+RealGoose@users.noreply.github.com>
2020-09-26 09:11:28 -0700

    Add files via upload
    Run Final.py from File Explorer to start

---
+++ hospital.py
@@ -0,0 +1,36 @@
+import http.client
+import mimetypes
+import json
+from distanceRequest import getDistance
+def getHospList(latitude, longitude):
+    """
+    Prints a list of formatted addresses of nearby hospitals
+    """
+    conn = http.client.HTTPSConnection("api.radar.io")
+    payload = ''
+    headers = {
+      'Authorization': 'prj_test_pk_6e76504d47441be5e8ad2fc0dcd6daaa57083aa5 '
+    }
+
+
+    conn.request("GET", "/v1/search/autocomplete?query=hospital&near=" + str(latitude) + "," + str(longitude) + "&limit=5", payload, headers)
+    res = conn.getresponse()
+    data = res.read().decode("utf-8")
+
+
+    json_data = json.loads(data)
+
+    #print(json_data)
+    addresses = json_data["addresses"]
+
+    d_list = []
+
+    formatted_address_list = []
+    for address in addresses:
+        formatted_address_list.append(address["formattedAddress"])
+        #d_list.append((address["latitude"],address["longitude"]))
+        getDistance(latitude, longitude, address["latitude"], address["longitude"])
+
+    #print(d_list)
+    #print(formatted_address_list)
+    return formatted_address_list
